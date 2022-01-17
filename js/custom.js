b6bd5f10fa456f5f2e4f1b8ad70cc6a2711b9b49
Alejandro Javier Rebecchi <cheverebe@users.noreply.github.com>
2022-01-17 01:02:35 +0100

    map2

--- js/custom.js
+++ js/custom.js
@@ -64,12 +64,15 @@ $('.slider_btn_next').on('click', function (e) {
 })
 
 
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+});
